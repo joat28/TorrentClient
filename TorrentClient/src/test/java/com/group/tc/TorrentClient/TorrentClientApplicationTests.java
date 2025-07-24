e21d7461bbd35025d995146c6c418e232e916acc
Prabhat Rao <prabhat.rao@iitg.ac.in>
2025-07-24 21:21:37 +0530

    init commit

---
+++ TorrentClient/src/test/java/com/group/tc/TorrentClient/TorrentClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.tc.TorrentClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorrentClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
