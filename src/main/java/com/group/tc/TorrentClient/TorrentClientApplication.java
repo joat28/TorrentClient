249549851716650585200ae84626ce9dac1bb1d5
Prabhat Rao <prabhat.rao@iitg.ac.in>
2025-07-26 21:12:45 +0530

    updating the directory setup, and moving git to parent directory, to be pushed into main

---
+++ src/main/java/com/group/tc/TorrentClient/TorrentClientApplication.java
@@ -0,0 +1,13 @@
+package com.group.tc.TorrentClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorrentClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorrentClientApplication.class, args);
+	}
+
+}
