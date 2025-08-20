eef8cabd5e3cca832406eff78c3fd7fec91f06e9
Prabhat Rao <prabhat.rao@iitg.ac.in>
2025-08-20 23:02:07 +0530

    Divide into microservices

---
+++ dashboard/src/test/java/com/group/tc/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.tc.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ swarm-service/src/test/java/com/group/tc/swarm/SwarmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.tc.swarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwarmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
